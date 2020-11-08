cf2424eb74367070b4c97d1d486195442d65f39e
ricoagustinus123 <rico_a.m@outlook.com>
2020-11-08 16:55:50 +0700

    rico 081120 16:52

--- updater.js
+++ updater.js
@@ -1,13 +1,16 @@
 import Navbar from "./components/navbar/Navbar.js";
 import Hero from "./components/hero/Hero.js";
+import Recent from "./components/RecentNews/Recent.js";
+import Footer from "./components/footer/Footer.js";
 
 const updater = () => {
   document.getElementById("root").innerHTML = `
         ${Navbar()}
         ${Hero()}
+        ${Recent()}
+        ${Footer()}
     `;
 };
-
 updater();
 
 export default updater;
